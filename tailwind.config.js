52acdca99559732c20506758a5107cbf8218a0cd
rafatparween <rafatparween27392@gmail.com>
2025-03-01 23:55:01 +0530

    domain chanes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '360px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      fontFamily: {
+        'sans': ['Roboto', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
